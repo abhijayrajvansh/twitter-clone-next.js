2d00ea216f61ee8a920ce88af9f6a5fd5fcf3d71
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-03 20:26:57 +0530

    update: navigation bar ✅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#1D9BF0',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
