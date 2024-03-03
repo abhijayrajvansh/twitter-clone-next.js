6d799ebb95fc9a4fffe88a8980f71931e1236df2
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-04 05:12:42 +0530

    update: audit push from local env

--- components/TrendingSection.tsx
+++ components/TrendingSection.tsx
@@ -8,11 +8,11 @@ const TrendingSection = () => {
   const [isInputFocused, setIsInputFocused] = useState(false);
 
   return (
-    <section className="flex flex-col fixed ml-[900px] w-full max-w-[375px] px-3 py-1">
+    <section className="flex flex-col fixed ml-[900px] w-full max-w-[375px] px-5 py-1">
       
       <div className="flex w-full items-center relative focus-within:text-primary">
         <IoSearch className={`text-xl ${isInputFocused ? 'text-primary' : 'text-faded'} absolute left-5`} />
-        <input className="text-md bg-transparent py-2.5 px-14 w-full bg-[#23272b] rounded-full forcus:border focus:border-primary focus:outline focus:bg-black" 
+        <input className={`text-md py-2.5 px-14 w-full rounded-full ${isInputFocused ? 'bg-black border border-primary outline-none':'bg-[#222529]'}`}
           placeholder="Search" 
           // onFocus: This event handler is triggered when an element gains focus, meaning when a user clicks or tabs into the element.
           // onBlur: This event handler is triggered when an element loses focus, meaning when a user clicks or tabs out of the element.
