2fe75c5ef1d29b273c2a8cdc26077791545b45a2
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-28 20:56:44 +0530

    update: copyright developer

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
-import { FaLinkedin } from "react-icons/fa";
+import { TfiWorld } from "react-icons/tfi";
 
 export default function Copyright () {
   return (
     <section className="mt-3 flex text-faded px-4 justify-center mb-10">
       {/* target="_blank" rel="noopener noreferrer" */}
-      <Link href='https://linkedin.com/in/abhijayrajvansh' target="_blank" rel="noopener noreferrer">
+      <Link href='https://abhijayrajvansh.com' target="_blank" rel="noopener noreferrer">
         <p className="font-medium flex items-center gap-2 ">
-          <FaLinkedin className="text-lg text-faded" />
-          Abhijay Rajvansh
+          <TfiWorld className="text-lg text-faded" />
+          <p className="hover:text-sky-500 hover:underline underline-offset-4 transition">Abhijay Rajvansh</p>
         </p>
       </Link>
       <Link href={`https://github.com/abhijayrajvansh/twitter-clone-next.js`} target="_blank" rel="noopener noreferrer">
