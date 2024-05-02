e4b176ac89109b9241604ae1f41e81acb3e6e2dc
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-02 08:06:34 +0530

    fix: hydration

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -4,15 +4,13 @@ import { TfiWorld } from "react-icons/tfi";
 export default function Copyright () {
   return (
     <section className="mt-3 flex text-faded px-4 justify-center mb-10">
-      {/* target="_blank" rel="noopener noreferrer" */}
       <Link href='https://abhijayrajvansh.com' target="_blank" rel="noopener noreferrer">
         <p className="font-medium flex items-center gap-2 ">
           <TfiWorld className="text-lg text-faded" />
-          <p className="hover:text-sky-500 hover:underline underline-offset-4 transition">Abhijay Rajvansh</p>
+          <span className="hover:text-sky-500 hover:underline underline-offset-4 transition">Abhijay Rajvansh</span>
         </p>
       </Link>
-      <Link href={`https://github.com/abhijayrajvansh/twitter-clone-next.js`} target="_blank" rel="noopener noreferrer">
-        <p>, view source code.</p>
+      <Link href={`https://github.com/abhijayrajvansh/twitter-clone-next.js`} target="_blank" rel="noopener noreferrer">, view source code.
       </Link>
     </section>
   )
