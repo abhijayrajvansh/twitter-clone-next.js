f3406a37e712c82d5671a4391a4efb908a246f76
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-05 21:41:09 +0530

    fixed: profile image tag

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image";
 import { MdMoreHoriz } from "react-icons/md";
 
 const Profile = () => {
   return (
     <div className="flex justify-between items-center px-4 py-3 cursor-pointer rounded-full mb-3">
         <div className="flex gap-3">
-          <img src="./images/elonpfp.png" alt="userpp" 
-            className="h-10 rounded-full" />
+
+          <Image width={40} height={40} 
+            src="/images/elonpfp.png" alt="userpp" 
+            className="h-10 rounded-full" 
+          />
+
           <div className="hidden lg:block">
-            <p className=" text-base font-bold">Elon Musk</p>
-            <p className="text-gray-400">@elonmusk</p>
+            <p className=" text-sm font-bold">Abhijay Rajvansh</p>
+            <p className="text-gray-400 text-sm">@elonmusk</p>
           </div>
         </div>
           <div className="hidden lg:block"><MdMoreHoriz className="text-2xl"/></div>
