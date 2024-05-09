920051275e8b1925fd6f56d80bd19bfa1d7ffc5e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-09 17:03:06 +0530

    update: audit fixes and changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const page = () => {
   return (
-    <div className="mt-10 flex flex-col items-center">
-      <Link href={'/home'}>
-        <button className="mt-5 text-xl w border-2 border-gray-700 rounded-lg py-4 px-8 hover:bg-gray-500/30">
-        Home
-        </button>
-      </Link>
-      <p className="text-xl mt-5">click on home or goto /home for feed.</p>
-      
+
+    <div className="flex flex-col">
+      <div className="flex flex-col mt-5 items-center">
+        <Link href={'/home'}>
+          <button className="text-xl w border-2 border-gray-700 rounded-lg py-4 px-8 hover:bg-gray-500/30">
+          Home
+          </button>
+        </Link>
+      </div>
+      <div>
+      <Image height={900} width={1280} className="max-w-7xl h-[90vh]" src={'/x-landingPage.png'} alt="landing page of x.com"/>
+      </div>
     </div>
   )
 }
