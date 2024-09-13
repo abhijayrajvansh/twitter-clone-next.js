a999b5c7129e3800e111d8aa6d8cf91c028b1e37
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-13 16:15:10 +0530

    wagmi: bug fixes & improvements

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,16 +4,13 @@ import Link from "next/link";
 const page = () => {
   return (
 
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-center container h-screen">
       <div className="flex flex-col mt-5 items-center">
         <Link href={'/home'}>
-          <button className="text-xl w border-2 border-gray-700 rounded-lg py-4 px-8 hover:bg-gray-500/30">
+          <button className="text-xl border-2 border-gray-700 rounded-lg py-2 px-8 hover:bg-gray-500/30">
           Home
           </button>
         </Link>
-      </div>
-      <div>
-      <Image height={900} width={1280} className="max-w-7xl h-[90vh]" src={'/x-landingPage.png'} alt="landing page of x.com"/>
       </div>
     </div>
   )
