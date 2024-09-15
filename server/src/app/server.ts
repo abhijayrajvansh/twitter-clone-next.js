e881845e43f5ef14ecc95343f913c1622474af26
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-15 21:47:58 +0530

    update: added cors

--- server/src/app/server.ts
+++ server/src/app/server.ts
@@ -2,10 +2,12 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import express from "express";
 import { User } from "../graphql/user";
+import cors from 'cors';
 
 export async function initGraphqlServer() {
   const app = express();
   app.use(express.json());
+  app.use(cors())
 
   const graphqlServer = new ApolloServer({
     typeDefs: `
