6e7363a5723bc3337fcb2b52a83e918a34136e2b
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-22 09:19:11 +0530

    update: improved copyright

--- client/components/Copyright.tsx
+++ client/components/Copyright.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link"
-import { TfiWorld } from "react-icons/tfi";
+import { FaGithub } from "react-icons/fa";
 
 export default function Copyright () {
   return (
-    <section className="mt-3 flex text-faded px-4 justify-center mb-10">
-      <Link href='https://abhijayrajvansh.com' target="_blank" rel="noopener noreferrer">
-        <p className="font-medium flex items-center gap-2 ">
-          <TfiWorld className="text-lg text-faded" />
-          <span className="text-sm hover:text-sky-500 hover:underline underline-offset-4 transition">Abhijay Rajvansh</span>
+    <section className="mt-3 flex flex-col items-center text-faded px-4 justify-center mb-10">
+      <Link href='https://abhijayrajvansh.com' target="_blank" rel="noopener noreferrer" className="hover:underline underline-offset-4 hover:text-sky-500">
+        <p className="font-medium flex items-center gap-1 text-sm">Developed by Abhijay Rajvansh
         </p>
       </Link>
-      <Link className="text-sm" href={`https://github.com/abhijayrajvansh/twitter-clone-next.js`} target="_blank" rel="noopener noreferrer">, view source code.
+      <Link className="text-sm flex items-center mt-1 gap-2 hover:text-white/70 hover:underline underline-offset-4" href={`https://github.com/abhijayrajvansh/twitter-clone-next.js`} target="_blank" rel="noopener noreferrer"><FaGithub size={18}/>view source code.
       </Link>
     </section>
   )
